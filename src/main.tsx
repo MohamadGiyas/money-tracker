51e36827cf9a70e7b6b68a7ec345a6a18c17ef1a
MohamadGiyas <147289838+MohamadGiyas@users.noreply.github.com>
2025-11-15 16:23:25 +0700

    fix: adjust import paths for deployment

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./pages/login";
-import Register from "./pages/register";
-import Dashboard from "./pages/dashboard";
-import ForgotPassword from "./pages/Forgot";
+
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import Forgot from "./pages/Forgot";
 import ResetPassword from "./pages/ResetPassword";
+
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/forgot" element={<Forgot />} />
         <Route path="/reset-password" element={<ResetPassword />} />
       </Routes>
     </BrowserRouter>
